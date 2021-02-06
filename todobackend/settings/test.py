2a498aa379d645ab2d663f5957f9aace76593706
pklata <pawelklata@gmail.com>
2021-02-06 11:22:21 +0100

    tests

---
+++ todobackend/settings/test.py
@@ -0,0 +1,32 @@
+from .base import *
+import os
+
+INSTALLED_APPS += ('django_nose',)
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR', '.')
+NOSE_ARGS = [
+    '--verbosity=2',
+    '--nologcapture',
+    '--with-coverage',
+    '--cover-package=todo',
+    '--with-spec',
+    '--spec-color',
+    '--with-xunit',
+    f'--xunit-file={TEST_OUTPUT_DIR}/unittests.xml',
+    '--cover-xml',
+    f'--cover-xml-file={TEST_OUTPUT_DIR}/coverage.xml'
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE', 'todobackend'),
+        'USER': os.environ.get('MYSQL_USER', 'todo'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+        'TEST': {
+            'NAME': 'todobackend',
+        },
+    }
+}
